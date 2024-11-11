c2e6d502fa7307aed26374692d8d46998edbc9d2
Samuel Obi <samobi588@gmail.com>
2024-11-11 15:44:32 +0100

    feat: added a line of code in server.js so that vercel can access the views folder

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.set("view engine", "ejs");
 //Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set('views', path.join(__dirname, 'views'));
+
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
